e54f63c9b34b30a8baf0150feeea76a4cf729994
o.chkhalo <o.chkhalo@sysiq.com>
2013-08-03 17:21:25 +0300

    Работающий курл

--- app/code/local/Los312/Async/Model/Adapter/Curl.php
+++ app/code/local/Los312/Async/Model/Adapter/Curl.php
@@ -9,7 +9,7 @@ class Los312_Async_Model_Adapter_Curl
         $this->multipleThreadsRequest($blocks);
     }
 
-    public function multipleThreadsRequest($blocks)
+    public function multipleThreadsRequest2($blocks)
     {
         
         $message = '|    Start Multycurl';
@@ -65,4 +65,50 @@ class Los312_Async_Model_Adapter_Curl
         return $res;
         
     }
+   /*working*/ 
+ public function multipleThreadsRequest($blocks)
+    {
+        $mh = curl_multi_init();
+        $curl_array = array();
+        foreach ($blocks as $identifer => $block) {
+            $url = Mage::helper('core/url')->getCurrentUrl();
+            $url = Mage::helper('core/url')->addRequestParam($url, array('async_block_identifer' => $block->getCacheKey()));
+            self::$remouteUrls[$identifer] = $url;
+            
+           // Mage::log('multipleThreadsRequest::url '.$url);
+
+            $curl_array[$identifer] = curl_init($url);
+            curl_setopt($curl_array[$identifer], CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($curl_array[$identifer], CURLOPT_TIMEOUT, 100);
+            //curl_setopt($curl_array[$identifer], CURLOPT_TIMEOUT_MS, 300);
+            curl_multi_add_handle($mh, $curl_array[$identifer]);
+        }
+        $running = NULL;
+        //curl_multi_exec($mh, $running);
+        //return;
+        $time = 0;
+        Mage::log('CURL start=================');
+        //curl_multi_exec($mh, $running);
+        do {
+            $time += 100000 ;
+            Mage::log('time:'.$time);
+            usleep(100000);
+            curl_multi_exec($mh, $running);
+        } while ($running > 0);
+//
+        $res = array();
+        foreach ($blocks as $identifer => $block) {
+            $res[$identifer] = curl_multi_getcontent($curl_array[$identifer]);
+        }
+        foreach ($blocks as $identifer => $block) {
+            curl_multi_remove_handle($mh, $curl_array[$identifer]);
+        }
+        curl_multi_close($mh);
+        /*res don't use*/
+        Mage::log('CURL CLOSE=================');
+        
+//        var_dump($res);
+//        die;
+        return $res;
+    }    
 }
